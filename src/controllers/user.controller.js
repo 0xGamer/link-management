643f2445e51d13c3c19de050cb6f1c281af5f6f1
Ujjwal <epicujjwal@protonmail.com>
2022-06-28 23:33:52 -0700

    controllers: add post, getone and getall users

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,18 +2,48 @@ const User = require('../models/user.model')
 
 
 // Gets all the users
-exports.getAllUsers = (req, res, next) => {
-  res.send("test");
+exports.getAllUsers = async (req, res, next) => {
+  try { 
+    let users = await User.find()
+    res.status(200).json(users)
+  } catch (e) {
+    res.statusCode = 404
+    next(e)
+  }
 };
 
 // Gets a single user with the specified :id
-exports.getUser = (req, res, next) => {
-  res.send("test");
+exports.getUser = async (req, res, next) => {
+  if (!req.params.id) {
+    res.statusCode = 400
+    next(new Error("Please Specify the user id"))
+  }
+  try {
+    let user = await User.findById(req.params.id)
+    res.status(200).json(user)
+  } catch (e) {
+    res.statusCode = 404
+    next(new Error(`User not found: ${req.params.id}`))
+  }
 };
 
 // Adds a new user
-exports.addUser = (req, res, next) => {
-  res.send("test");
+exports.addUser = async (req, res, next) => {
+  let user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  })
+
+  try {
+    await user.save()
+    console.log(user)
+    res.status(201).json(user)
+  } catch (e) {
+    // catches validation and schema errors and sends to err handler
+    res.statusCode = 400
+    next(e)
+  }
 };
 
 // Updates existing user with the specified :id
