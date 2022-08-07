b40884074fe6724543f18f97660902b5f66350ff
0xGamer <rishirajbehki11@gmail.com>
2022-08-07 19:37:01 +0530

    ujjwal is dumb

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { MantineProvider } from "@mantine/core";
+import { MantineProvider } from '@mantine/core';
 import { NotificationsProvider } from "@mantine/notifications";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
