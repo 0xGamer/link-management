9265d9c204193151fecd5aa85d4d04f7b224fcf1
0xGamer <rishirajbehki11@gmail.com>
2022-08-08 11:13:04 +0530

    a

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,104 +1,95 @@
 import { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Center, Container, TextInput, Button, Table } from "@mantine/core";
-import Service from "./services/user.service";
+import { Center, Container, TextInput, Button, Table, Checkbox  } from "@mantine/core";
+import Service from "./services/link.service";
 
 function App() {
   useEffect(() => {
-    getUsers();
+    getLinks();
   }, []);
 
-  let [name, setName] = useState();
-  let [email, setMail] = useState();
-  let [password, setPassword] = useState();
+  let [linkurl, setLinkurl] = useState();
+  let [file, setFile] = useState();
+  let [form, setForm] = useState();
 
   let [updateActive, setUpdateActive] = useState(false);
   let [updateId, setUpdateId] = useState()
 
-  let [users, setUsers] = useState([]);
-  const getUsers = async () => {
+  let [links, setLinks] = useState([]);
+  const getLinks = async () => {
     try {
-      let users = await Service.getAllUsers();
-      setUsers(users.data);
+      let links = await Service.getAllLinks();
+      setLinks(links.data);
     } catch {
-      alert("Cannot fetch users");
+      alert("Cannot fetch links");
     }
   };
 
-  const addUser = async () => {
+  const addLink = async () => {
     try {
-      let user = { name, email, password };
-      await Service.addUser(user);
-      setMail("");
-      setName("");
-      setPassword("");
-      getUsers();
-    } catch {
-      alert("Error creating user");
+      let link = { linkurl, file, form };
+      await console.log(linkurl)
+      await Service.addLink(link);
+      setFile(false);
+      setLinkurl("");
+      setForm(false);
+      getLinks();
+    } catch(e) {
+      console.log(e);
     }
   };
 
-  const deleteUser = async (id) => {
+  const deleteLink = async (id) => {
     try {
-      await Service.deleteUser(id);
-      getUsers();
+      await Service.deleteLink(id);
+      getLinks();
     } catch {
-      alert("Could not delete user");
+      alert("Could not delete link");
     }
   };
 
   const activateUpdate = async (element) => {
-    setName(element.name)
-    setMail(element.email)
-    setPassword(element.password)
+    setLinkurl(element.linkurl)
+    setFile(element.file)
+    setForm(element.form)
     setUpdateId(element._id)
     setUpdateActive(true)
   }
 
-  const updateUser = async () => {
+  const updateLink = async () => {
     try {
-      let user = { name, email, password };
-      await Service.updateUser(updateId, user);
-      setMail("");
-      setName("");
-      setPassword("");
+      let link = { linkurl, file, form };
+      await Service.updateLink(updateId, link);
+      setFile(false);
+      setLinkurl("");
+      setForm(false);
       setUpdateActive(false)
-      getUsers();
+      getLinks();
     } catch {
-      alert("Error creating user");
+      alert("Error creating link");
     }
   };
 
   return (
     <div className="App">
-      <h1 style={{ marginTop: 5 + `rem` }}>User Management App</h1>
+      <h1 style={{ marginTop: 5 + `rem` }}>Link Management App</h1>
       <Container style={{ width: 40 + "%" }}>
         <TextInput
-          placeholder="Name"
-          value={name}
-          onChange={(e) => setName(e.currentTarget.value)}
+          placeholder="Linkurl"
+          value={linkurl}
+          onChange={(e) => setLinkurl(e.currentTarget.value)}
           required
         />
         <br />
-        <TextInput
-          placeholder="email"
-          value={email}
-          onChange={(e) => setMail(e.currentTarget.value)}
-          required
-        />
+        <Checkbox checked={file} onChange={(event) => setFile(event.currentTarget.checked)} color="dark" label="File"/>
         <br />
-        <TextInput
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.currentTarget.value)}
-          required
-        />
+        <Checkbox checked={form} onChange={(event) => setForm(event.currentTarget.checked)} color="dark" label="Form"/>
         <br />
         {updateActive ? 
 
         <Button
-          onClick={updateUser}
+          onClick={updateLink}
           variant="gradient"
           gradient={{ from: "teal", to: "blue", deg: 60 }}
         >
@@ -106,11 +97,11 @@ function App() {
         </Button>
           :
         <Button
-          onClick={addUser}
+          onClick={addLink}
           variant="gradient"
           gradient={{ from: "teal", to: "blue", deg: 60 }}
         >
-          Add User
+          Add Link
         </Button>
         }
 
@@ -121,22 +112,22 @@ function App() {
         <Table striped highlightOnHover>
           <thead>
             <tr>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Password</th>
+              <th>Linkurl</th>
+              <th>File</th>
+              <th>Form</th>
               <th>Delete</th>
               <th>Update</th>
             </tr>
           </thead>
           <tbody>
-            {users.map((element) => {
+            {links.map((element) => {
               return (
                 <tr key={element._id.toString()}>
-                  <td>{element.name}</td>
-                  <td>{element.email}</td>
-                  <td>{element.password}</td>
+                  <td>{element.linkurl}</td>
+                  <td>{element.file}</td>
+                  <td>{element.form}</td>
                   <td>
-                    <Button color="red" onClick={() => deleteUser(element._id)}>
+                    <Button color="red" onClick={() => deleteLink(element._id)}>
                       Delete
                     </Button>
                   </td>
--- client/src/services/link.service.js
+++ client/src/services/link.service.js
@@ -1,20 +1,20 @@
-const URL = "https://node-api-xyz.herokuapp.com/users";
+const URL = "https://school-api-billabong.herokuapp.com/links";
 import axios from "axios";
 
 const Service = {
-  getAllUsers: async function () {
+  getAllLinks: async function () {
     return await axios.get(URL);
   },
 
-  addUser: async function (data) {
+  addLink: async function (data) {
     return axios.post(URL, data);
   },
 
-  updateUser: async function (id, data) {
+  updateLink: async function (id, data) {
     return axios.patch(`${URL}/${id}`, data);
   },
 
-  deleteUser: async function (id) {
+  deleteLink: async function (id) {
     return axios.delete(`${URL}/${id}`);
   },
 };
